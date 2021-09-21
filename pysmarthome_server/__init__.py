9ec7c5e2c728001a9bb54c98a7af387a35527b0b
Filipe Alves <filipe.alvesdefernando@hotmail.com>
2021-09-21 13:02:23 -0300

    refactor: use PluginManager handlers to instead of watchdog

--- pysmarthome_server/__init__.py
+++ pysmarthome_server/__init__.py
@@ -6,7 +6,7 @@ from pysmarthome import PluginManager
 from .db import db
 from .config import config
 from .graphql import mkschema
-from .watchdog.watcher import reset_on_package_updates
+from .handlers import Scheduler
 from multiprocessing import Process
 
 
@@ -15,7 +15,11 @@ def start_server(**config):
     app.config['API_KEY'] = config['api_key']
     register_middlewares(app)
     conn = db.init(config['db'])
-    dev_controllers = PluginManager.init(conn)
+    dev_controllers = PluginManager.init(conn, handlers={
+        'on_install': Scheduler.schedule_restart,
+        'on_uninstall': Scheduler.schedule_restart,
+        'on_toggle_active': Scheduler.schedule_restart,
+    })
     schema = mkschema([ctrl.model_class for ctrl in dev_controllers.values()])
 
     @app.route('/graphql', methods=['GET'])
@@ -50,5 +54,4 @@ def start_server(**config):
 
 
 app_process = Process(target=start_server, kwargs=config)
-reset_on_package_updates(app_process)
 start_server = app_process.start
